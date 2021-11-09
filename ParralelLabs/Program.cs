f39c3ce1928e083fd6dd2477c6b30e2deabc2c08
bunyamin <42899786+MetaMmodern@users.noreply.github.com>
2021-11-09 12:11:02 +0200

    first commit

---
+++ ParralelLabs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace ParralelLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Hello World Thread";
+            Console.WriteLine(Thread.CurrentThread.Name);
+            return;
+        }
+    }
+}
