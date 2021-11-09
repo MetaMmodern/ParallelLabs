3ab28f1c9f3d8151fb93aea753c015638ab8690e
bunyamin <42899786+MetaMmodern@users.noreply.github.com>
2021-11-09 22:56:45 +0200

    Added base structure

--- ParralelLabs/Program.cs
+++ ParralelLabs/Program.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace ParralelLabs
 {
+    class SkipList {
+        public List<LinkedList<int>> layers;
+        public SkipList()
+        {
+            layers = new List<LinkedList<int>>();
+        }
+        private int AddLayer() {
+            var layerLevel = 0; 
+            return layerLevel;
+        }
+        private void DeleteLayer()
+        {
+            var layerLevel = 0;
+        }
+        public void Add(int value) { }
+        public int Get(int value) { return 0; }
+        public void Delete() { }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -12,6 +32,14 @@ namespace ParralelLabs
             mainThread.Name = "Hello World Thread";
             Console.WriteLine(Thread.CurrentThread.Name);
             return;
+        }
+
+        bool Cas(ref int a, int oldValue, int newValue) {
+            if (a != oldValue)
+                return false;
+            a = newValue;
+            return true;
+
         }
     }
 }
